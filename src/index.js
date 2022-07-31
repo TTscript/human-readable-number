0ba14d42387d22e701a9bb757b755835b4bd64d5
Timur <75645669+TTscript@users.noreply.github.com>
2022-07-31 21:53:00 +0600

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const wholeTens = ['wholeTensZero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const hundreds = ['hundredsZero', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
+
+    const numberStr = number.toString();
+
+    if (numberStr.length === 1) {
+        console.log(units[number]);
+        return units[number];
+    } else if (numberStr.length === 2) {
+        if ([...numberStr][0] === '1') {
+            console.log(tens[parseInt([...numberStr][1])]);
+            return tens[parseInt([...numberStr][1])];
+        } else if (parseInt([...numberStr][0], 10) > 1) {
+            if (parseInt([...numberStr][1], 10) === 0) {
+                console.log(wholeTens[parseInt([...numberStr][0])]);
+                return wholeTens[parseInt([...numberStr][0])];
+            }
+            console.log(`${wholeTens[parseInt([...numberStr][0])]} ${units[parseInt([...numberStr][1])]}`);
+            return `${wholeTens[parseInt([...numberStr][0])]} ${units[parseInt([...numberStr][1])]}`;
+        }
+    } else if (numberStr.length === 3) {
+        if (parseInt([...numberStr][0], 10) > 0) {
+            if (parseInt([...numberStr][1], 10) === 0 && parseInt([...numberStr][2], 10) === 0) {
+                console.log(hundreds[parseInt([...numberStr][0])]);
+                return hundreds[parseInt([...numberStr][0])];
+            } else if (parseInt([...numberStr][1], 10) === 1) {
+                console.log(`${hundreds[parseInt([...numberStr][0])]} ${tens[parseInt([...numberStr][2])]}`);
+                return `${hundreds[parseInt([...numberStr][0])]} ${tens[parseInt([...numberStr][2])]}`;
+            } else if (parseInt([...numberStr][1], 10) > 1 && parseInt([...numberStr][2], 10) === 0) {
+                console.log(`${hundreds[parseInt([...numberStr][0])]} ${wholeTens[parseInt([...numberStr][1])]}`);
+                return `${hundreds[parseInt([...numberStr][0])]} ${wholeTens[parseInt([...numberStr][1])]}`;
+            } else if (parseInt([...numberStr][1], 10) === 0) {
+                console.log(`${hundreds[parseInt([...numberStr][0])]} ${units[parseInt([...numberStr][2])]}`);
+                return `${hundreds[parseInt([...numberStr][0])]} ${units[parseInt([...numberStr][2])]}`;
+            }
+            console.log(`${hundreds[parseInt([...numberStr][0])]} ${wholeTens[parseInt([...numberStr][1])]} ${units[parseInt([...numberStr][2])]}`);
+            return `${hundreds[parseInt([...numberStr][0])]} ${wholeTens[parseInt([...numberStr][1])]} ${units[parseInt([...numberStr][2])]}`;
+        }
+    }
 }
